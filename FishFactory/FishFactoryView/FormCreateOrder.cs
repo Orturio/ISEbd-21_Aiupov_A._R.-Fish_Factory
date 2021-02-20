8879c2d96654c5aeb006ae3ecc2d47505449f8ca
gusevsvoloch@gmail.com <gusevsvoloch@gmail.com>
2021-02-20 11:26:40 +0400

    update

--- FishFactory/FishFactoryView/FormCreateOrder.cs
+++ FishFactory/FishFactoryView/FormCreateOrder.cs
@@ -30,10 +30,10 @@ namespace FishFactoryView
                 var list = _logicP.Read(null);
                 foreach (var item in list)
                 {
-                    comboBoxProduct.DisplayMember = "CannedName";
-                    comboBoxProduct.ValueMember = "Id";
-                    comboBoxProduct.DataSource = list;
-                    comboBoxProduct.SelectedItem = null;
+                    comboBoxCanned.DisplayMember = "CannedName";
+                    comboBoxCanned.ValueMember = "Id";
+                    comboBoxCanned.DataSource = list;
+                    comboBoxCanned.SelectedItem = null;
                 }
             }
             catch (Exception ex)
@@ -45,12 +45,12 @@ MessageBoxIcon.Error);
 
         private void CalcSum()
         {
-            if (comboBoxProduct.SelectedValue != null &&
+            if (comboBoxCanned.SelectedValue != null &&
 !string.IsNullOrEmpty(textBoxCount.Text))
             {
                 try
                 {
-                    int id = Convert.ToInt32(comboBoxProduct.SelectedValue);
+                    int id = Convert.ToInt32(comboBoxCanned.SelectedValue);
                     CannedViewModel product = _logicP.Read(new CannedBindingModel{Id = id})?[0];
                     int count = Convert.ToInt32(textBoxCount.Text);
                     textBoxSum.Text = (count * product?.Price ?? 0).ToString();
@@ -69,7 +69,7 @@ MessageBoxIcon.Error);
             CalcSum();
         }
 
-        private void ComboBoxProduct_SelectedIndexChanged(object sender, EventArgs e)
+        private void ComboBoxCanned_SelectedIndexChanged(object sender, EventArgs e)
         {
             CalcSum();
         }
@@ -83,7 +83,7 @@ MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
-            if (comboBoxProduct.SelectedValue == null)
+            if (comboBoxCanned.SelectedValue == null)
             {
                 MessageBox.Show("Выберите изделие", "Ошибка", MessageBoxButtons.OK,
 MessageBoxIcon.Error);
@@ -94,7 +94,7 @@ MessageBoxIcon.Error);
             {
                 _logicO.CreateOrder(new CreateOrderBindingModel
                 {
-                    CannedId = Convert.ToInt32(comboBoxProduct.SelectedValue),
+                    CannedId = Convert.ToInt32(comboBoxCanned.SelectedValue),
                     Count = Convert.ToInt32(textBoxCount.Text),
                     Sum = Convert.ToDecimal(textBoxSum.Text)
                 });
