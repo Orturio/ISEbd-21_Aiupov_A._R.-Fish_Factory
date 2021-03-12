95555e40e2925c2008c459397cf582de9f4b75b0
gusevsvoloch@gmail.com <gusevsvoloch@gmail.com>
2021-03-12 14:46:55 +0400

    update

---
+++ FishFactory/FishFactoryView/FormWerehouseRestocking.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections.Generic;
+using FishFactoryBusinessLogic.ViewModels;
+using FishFactoryBusinessLogic.BusinessLogics;
+using System;
+using FishFactoryBusinessLogic.BindingModels;
+using FishFactoryFileImplement;
+using FishFactoryFileImplement.Implements;
+using FishFactoryFileImplement.Models;
+using System.Windows.Forms;
+using Unity;
+using System.Linq;
+
+namespace FishFactoryView
+{
+    public partial class FormWarehouseRestocking : Form
+    {
+        WarehouseStorage _warehouseStorage = new WarehouseStorage();
+
+        WarehouseBindingModel bm = new WarehouseBindingModel();
+
+        public string ComponentName { get { return comboBoxComponent.Text; } }
+
+        public int ComponentId
+        {
+            get { return Convert.ToInt32(comboBoxComponent.SelectedValue); }
+            set { comboBoxComponent.SelectedValue = value; }
+        }
+
+        public int WarehouseId
+        {
+            get { return Convert.ToInt32(comboBoxWarehouse.SelectedValue); }
+            set { comboBoxWarehouse.SelectedValue = value; }
+        }
+
+        public int Count
+        {
+            get { return Convert.ToInt32(textBoxCount.Text); }
+            set
+            {
+                textBoxCount.Text = value.ToString();
+            }
+        }
+
+        public FormWarehouseRestocking(ComponentLogic componentlogic, WarehouseLogic warehouseLogic)
+        {
+            InitializeComponent();
+            List<ComponentViewModel> listComponent = componentlogic.Read(null);
+            if (listComponent != null)
+            {
+                comboBoxComponent.DisplayMember = "ComponentName";
+                comboBoxComponent.ValueMember = "Id";
+                comboBoxComponent.DataSource = listComponent;
+                comboBoxComponent.SelectedItem = null;
+            }
+
+            List<WarehouseViewModel> listWarehouse = warehouseLogic.Read(null);
+            if (listWarehouse != null)
+            {
+                comboBoxWarehouse.DisplayMember = "WarehouseName";
+                comboBoxWarehouse.ValueMember = "Id";
+                comboBoxWarehouse.DataSource = listWarehouse;
+                comboBoxWarehouse.SelectedItem = null;
+            }
+        }
+
+        private void ButtonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxCount.Text))
+            {
+                MessageBox.Show("Заполните поле Количество", "Ошибка",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (comboBoxComponent.SelectedValue == null)
+            {
+                MessageBox.Show("Выберите компонент", "Ошибка", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+                return;
+            }
+            if (comboBoxWarehouse.SelectedValue == null)
+            {
+                MessageBox.Show("Выберите склад", "Ошибка", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+                return;
+            }
+            _warehouseStorage.Restocking(bm, WarehouseId, ComponentId, Count, ComponentName);
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
